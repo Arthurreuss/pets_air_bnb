c845006b486f81525a81337af340d5524ef18d57
Arthur Reuss <arthur.reuss@gmx.de>
2022-11-21 16:22:49 +0100

    setup finished

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,4 @@
 class Booking < ApplicationRecord
-  has_many :pets
+  belongs_to :pet
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_141724) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_145950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_141724) do
     t.integer "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["pet_id"], name: "index_bookings_on_pet_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -48,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_141724) do
   end
 
   add_foreign_key "bookings", "pets"
+  add_foreign_key "bookings", "users"
 end
