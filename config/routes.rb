f92b1ff2c8052fa2024565d0e9a26bef2cc0b0e5
Arthur Reuss <arthur.reuss@gmx.de>
2022-11-21 17:43:33 +0100

    quotationmarks added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "dashboard", to: "pages#dashboard", as: dashboard_path
+  get "dashboard", to: "pages#dashboard", as: "dashboard_path"
   # Defines the root path route ("/")
   # root "articles#index"
   # get "/index", to: "pets#index"
