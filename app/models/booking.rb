2a9a2a5d3799ccacfeafb2a2773dd6e818831681
Arthur Reuss <arthur.reuss@gmx.de>
2022-11-22 15:57:13 +0100

    address added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,17 +5,18 @@ class Booking < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
   # validates_comparison_of :start_date, less_than: :end_date, greater_than_or_equal_to: -> { Date.today }
-  
+
   validate :validate_start_date
- 
+
   private
 
   def validate_start_date
     if self.start_date < self.end_date && self.start_date >= Date.today
       return true
-    else
-      errors.add("Start date must be valid")
+    elsif self.start_date < Date.today
+      self.errors.add(:start_date, "Start date must be valid")
+    elsif self.start_date > self.end_date
+      self.errors.add(:end_date, "End date must be valid")
     end
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_145950) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_131746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_145950) do
     t.string "species"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
